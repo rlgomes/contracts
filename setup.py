5bf0d70969af880828db61a7627d433dd7e912a8
Rodney Lopes Gomes <rodneygomes@gmail.com>
2012-06-02 12:31:43 -0700

    - a few updates and wanted to test out travis-ci

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 """
 setup.py
 """
-from distutils.core import setup
+from setuptools import setup
 
 setup (
     name='contracts',
     version='0.1.0',
     author='Rodney Gomes',
-    author_email='rodneygomes@attention: gmail.com',
+    author_email='rodneygomes@gmail.com',
     url='',
+    install_requires = ["setuptools"],
+    test_suite="test",
     keywords = ['contracts'],
     py_modules = ['contracts'],
     license='Apache 2.0 License',
--- test/alltests.py
+++ test/alltests.py
@@ -8,15 +8,15 @@ from invtests import InvTests
 
 def suite():
     suite = unittest.TestSuite()
-    
+
     suite.addTest(unittest.makeSuite(BasicTests))
     suite.addTest(unittest.makeSuite(PreTests))
     suite.addTest(unittest.makeSuite(PostTests))
     suite.addTest(unittest.makeSuite(InvTests))
-    
+
 #    from performance import Performance
 #    suite.addTest(unittest.makeSuite(Performance))
-    
+
     return suite
 
 if __name__ == '__main__':
